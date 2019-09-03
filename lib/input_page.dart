4539a4d3854568e12af01f22344771c30efdd1ca
Jordan Gillard <j.gillard@gastrofix.com>
2019-09-03 18:48:59 +0200

    Moved input page to its own file and build out base buttons on main screen.

---
+++ lib/input_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: <Expanded>[
+          Expanded(
+            child: Row(
+              children: <Expanded>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: Colors.white,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              colour: Colors.white,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Expanded>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: Colors.white,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
