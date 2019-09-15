4e09ac372e234392592528b4947b2cfe669fafd6
Jordan Gillard <j.gillard@gastrofix.com>
2019-09-15 14:00:01 +0200

    refactored imports to not use relative imports

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,11 +1,11 @@
+import 'package:bmi_calculator/components/calculate_button.dart';
+import 'package:bmi_calculator/components/icon_content.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
+import 'package:bmi_calculator/constants.dart';
 import 'package:bmi_calculator/screens/results_page.dart';
 import 'package:bmi_calculator/components/round_icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import '../components/calculate_button.dart';
-import '../constants.dart';
-import '../components/icon_content.dart';
-import '../components/reusable_card.dart';
 
 enum Gender { male, female }
 
